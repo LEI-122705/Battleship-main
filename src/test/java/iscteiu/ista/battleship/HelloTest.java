c2979ba4a2b1ccd249e01d1d3ef86a8c4f53ca5e
cmsca3 <cmsca3@iscte-iul.pt>
2025-11-13 13:22:39 +0000

    Teste para saber se o workflow maven.yml corre com sucesso

---
+++ src/test/java/iscteiu/ista/battleship/HelloTest.java
@@ -0,0 +1,11 @@
+package iscteiu.ista.battleship;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloTest {
+    @Test
+    void alwaysPasses() {
+        assertTrue(true);
+    }
+}
